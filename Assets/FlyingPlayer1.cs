fb1a76e9a001b410f9e70c19b42cf8823dee738e
Achintya Bhatnagar <achintyb@amazon.com>
2019-12-05 19:27:11 +0530

    Scene1: Take2

--- Assets/FlyingPlayer1.cs
+++ Assets/FlyingPlayer1.cs
@@ -3,47 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class HarryPotterFlyingJat : MonoBehaviour
+public class FlyingPlayer1 : MonoBehaviour
 {
     private GameObject go;
 
     static int count = 0;
-   
+
     // Start is called before the first frame update
     void Start()
-    {    
+    {
         InvokeRepeating("moveHarryPotter", 2.0f, 0.05f);
+       
     }
-
 
 
 
     void moveHarryPotter()
     {
-       
 
-        // Move the object forward along its z axis 1 unit/second.
-        System.Random random = new System.Random();
+
+        // Move the object forward along its z axis 1 unit/second
 
         //count++;
         //if(count % 5 == 0 && count %10 !=0)
         //{
-          //  transform.Translate(Vector3.up * 5);
+        //  transform.Translate(Vector3.up * 5);
         //}
-        //if(count %10 ==0)
+        //if(count %10 ==0)ß
         //{
-          //  transform.Translate(Vector3.down * 5);
+        //  transform.Translate(Vector3.down * 5);
         //}
-        transform.Translate(Vector3.forward * random.Next(3,7));
+        if (count == 50)
+        {
+            count = 0;
+            transform.Rotate(0f, 0f, 180f, Space.Self);
+        }
+        transform.Translate(Vector3.right * 5f);
+       
+        count++;
 
 
         //transform.Translate(Vector3.right * 5);
 
-        transform.Rotate(0f, 3.0f, 0f, Space.Self);
+        
         // Move the object upward in world space 1 unit/second.
-       
+
     }
-  
+
     // Update is called once per frame
     void Update()
     {
