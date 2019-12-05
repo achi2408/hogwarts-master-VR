a4d1495b38ef25ec5cefdddd7e90b42222a4c3bf
Achintya Bhatnagar <achintyb@amazon.com>
2019-12-05 17:40:20 +0530

    Scene1: Take1

---
+++ Assets/HarryPotterFlyingJat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HarryPotterFlyingJat : MonoBehaviour
+{
+    private GameObject go;
+
+   
+   
+    // Start is called before the first frame update
+    void Start()
+    {    
+        InvokeRepeating("moveHarryPotter", 2.0f, 0.3f);
+    }
+
+    void moveHarryPotter()
+    {
+        // Move the object forward along its z axis 1 unit/second.
+        transform.Translate(Vector3.forward * 5);
+
+        // Move the object upward in world space 1 unit/second.
+        transform.Translate(Vector3.up * 5, Space.World);
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+        Console.WriteLine("This is C#");
+    }
+}
